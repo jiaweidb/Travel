8be0d1ee300e6169af7225ab6e48eedef76d1b20
jiaweidb <isjiawei@163.com>
2020-03-27 21:29:34 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template> 
+   <div class="header">
+       <div class="header-left">
+           <span class="iconfont back-icon">&#xe624;</span>
+       </div>
+       <div class="header-input">
+           <span class="iconfont">&#xe632;</span>
+           输入城市/景点/有游玩主题
+        </div>
+       <div class="header-right">
+           城市
+           <span class="iconfont arrow-icon">&#xe62a;</span>
+        </div>
+   </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    line-height: .86rem
+    display: flex
+    background: $bgcolor
+    color: #ffffff
+    .header-left
+      width: .64rem
+      float: left
+      padding-left: .12rem
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      background: #fff
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+
+</style>
