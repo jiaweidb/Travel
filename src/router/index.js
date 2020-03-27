845378775cc202cfbb75bd8821262f334326c2fd
jiaweidb <isjiawei@163.com>
2020-03-27 17:34:46 +0800

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/', // 当你访问根路径的时候，其实我展示的是Helloworld这个组件---->就App.vue + router + HelloWorld.vue这么一层层连上了且这两个组件的内容共同构成了http://localhost/#的内容
+      name: 'Home',
+      component: Home
     }
   ]
 })
