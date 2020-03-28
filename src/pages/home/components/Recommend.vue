c6b609125fa1dcf7612c09fbd151f0801a489b2d
jiaweidb <isjiawei@163.com>
2020-03-28 18:19:02 +0800

    add bottomstyles

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+      <div class="title">热门推荐</div>
+      <ul>
+          <li class="item" v-for="item of recommendList" :key="item.id">
+            <img class="item-img" :src="item.imgUrl">
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-btn">查看详细</button>
+            </div>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            recommendList: [{
+                id: '0001',
+                imgUrl: "https://imgs.qunarzz.com/sight/p0/1610/96/96dd898b88f35cc0a3.water.jpg_200x200_4885de47.jpg",
+                title: "景德镇古窑民俗博览区",
+                desc: "春暖花开 走出室内 体验你和它的乐趣"
+            },{
+                id: '0002',
+                imgUrl: "https://imgs.qunarzz.com/sight/p0/1610/96/96dd898b88f35cc0a3.water.jpg_200x200_4885de47.jpg",
+                title: "景德镇古窑民俗博览区",
+                desc: "春暖花开 走出室内 体验你和它的乐趣"
+            },{
+                id: '0003',
+                imgUrl: "https://imgs.qunarzz.com/sight/p0/1610/96/96dd898b88f35cc0a3.water.jpg_200x200_4885de47.jpg",
+                title: "景德镇古窑民俗博览区",
+                desc: "春暖花开 走出室内 体验你和它的乐趣"
+            },]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .1rem
+    padding: .1rem
+    background:#ccc
+    text-indent: .2rem
+    line-height: .48rem
+    height: .48rem
+  .item
+    display: flex
+    height: 1.9rem
+    .item-img
+      padding: .1rem
+      width: 1.7rem
+      height: 1.7rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        text-indent: .2rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-heigth: .4rem
+        color: #ccc
+        margin-top: .1rem
+        text-indent: .2rem
+        ellipsis()
+      .item-btn
+        margin-left: .2rem
+        margin-top: .23rem
+        line-height: .42rem
+        background: #ff9300
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,61 @@
+<template>
+   <div>
+        <div class="title">周末去哪游</div>
+        <ul>
+            <li class="item" v-for="item of weekendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl" />
+                </div>
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+            </li>
+        </ul>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data () {
+        return {
+            weekendList: [{
+                id: '0001',
+                imgUrl: "https://imgs.qunarzz.com/sight/source/1811/24/30f714149293b6.jpg_r_640x214_bfa37f12.jpg",
+                title: "狮子峰等你",
+                desc: "有黄山之险，悬崖峭壁，这里有满山的翠竹、青藤，堪称人间仙境世外桃源"
+            },{
+                id: '0002',
+                imgUrl: "https://imgs.qunarzz.com/sight/source/1606/44/19689f8a01f5b2.jpg_r_640x214_7959e855.jpg",
+                title: "九江打卡",
+                desc: "有2200多年历史的江南文化名城"
+            },{
+                id: '0003',
+                imgUrl: "https://imgs.qunarzz.com/sight/source/1505/bd/688c12889af7ad.jpg_r_640x214_cc490c32.jpg",
+                title: "happy乐园",
+                desc: "在这里玩转乐园，疯玩起来，无忧无虑一整天！"
+            },]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    line-height: .48rem
+    height: .48rem
+    background: #ccc
+    text-indent: .2rem
+    padding: .1rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-title
+    padding: .1rem
+    font-size: .32rem
+  .item-desc
+    padding: .1rem
+    color: #ccc
+</style>
