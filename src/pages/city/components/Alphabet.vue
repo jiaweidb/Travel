f5dd5a1ee9b0c3eb0f69769aca98a9d04f8757c5
jiaweidb <isjiawei@163.com>
2020-04-01 16:56:40 +0800

    finish city-list css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <ul>
+            <li class="item">A</li>
+            <li class="item">B</li>
+            <li class="item">C</li>
+            <li class="item">D</li>
+            <li class="item">E</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CityAlphabet',
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 1.58rem
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgcolor
+</style> 
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">您的位置</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">乐平</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+    name: 'CityList',
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #ccc 
+    &:after
+      border-color:  #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list /*要保证这个整个list不能滑动，所以设置absolute,并且设置overflow:hidden。top留值给输入框显示，*/
+    position: absolute
+    top: 1.63rem
+    right: 0
+    bottom: 0
+    left: 0
+    overflow: hidden
+    .title
+      line-height: .54rem
+      height: .5rem
+      background: #eee
+      text-indent: .2rem
+      font-size: .26rem
+      color: #666
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper /*这个wrapper的样式写的很简洁，你要注意了 */
+        float: left 
+        width: 33.33%
+        .button /*控制多个button间的间距在这里写了，其中想改变button的高度：即内容到上下边框的距离的时候使用了padding */
+          margin: .1rem
+          text-align: center
+          padding: .1rem 0
+          border-radius: .06rem
+          border: .02rem solid #ccc
+    .item-list
+      .item
+        line-height: .64rem
+        padding-left: .2rem
+</style> 
